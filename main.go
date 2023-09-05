1cf61c157907fe44ef8d7fb8b49d1952432271be
MrCharlss <mistercharls@gmail.com>
2023-09-04 21:10:39 -0600

    db connection working and some initial models

--- main.go
+++ main.go
@@ -3,14 +3,61 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	tgbot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"gorm.io/gorm"
 )
+
+type Category struct {
+	Name string
+	ID   uint `gorm:"primaryKey;autoIncrement"`
+}
+
+type Expence struct {
+	gorm.Model
+	ID         uint `gorm:"primaryKey;autoIncrement;"`
+	CategoryID int
+	Category   Category
+	Name       string
+	Amount     int
+}
+
+type BudgetItem struct {
+	gorm.Model
+	ID         uint `gorm:"primaryKey;autoIncrement;"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	Name       string
+	Amount     int
+	CategoryID int
+	Category   Category
+	Fortnight  int
+	Month      Month
+	MonthId    int
+}
+
+type Month struct {
+	Month string
+	ID    uint `gorm:"primaryKey;autoIncrement"`
+}
+
+var numericKeyBoard = tgbot.NewReplyKeyboard(tgbot.NewKeyboardButtonRow(tgbot.NewKeyboardButton("WOW")))
+var inlineKeyboard = tgbot.NewInlineKeyboardMarkup(tgbot.NewInlineKeyboardRow(tgbot.NewInlineKeyboardButtonData("N", "5")))
 
 func main() {
 	fmt.Println("Works...")
-    token := os.Getenv("TEL_TOKEN")
-    fmt.Println(token)
+	// db, error := gorm.Open(sqlite.Open("finance.db"), &gorm.Config{})
+	// error = db.AutoMigrate(&Category{}, &Expence{}, &BudgetItem{}, &Month{})
+	// if error != nil {
+	//     panic(error)
+	// }
+
+	// if error != nil {
+	//     panic(error)
+	// }
+	token := os.Getenv("TEL_TOKEN")
+	fmt.Println(token)
 	bot, err := tgbot.NewBotAPI(token)
 	if err != nil {
 		panic(err)
@@ -22,11 +69,27 @@ func main() {
 
 	for update := range updatesChan {
 		if update.Message == nil {
-			fmt.Printf("Message nil - %+v\n", update)
+			// fmt.Printf("Message nil - %+v\n", update)
 			continue
 		}
-		msg := tgbot.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
+		if !update.Message.IsCommand() {
+			continue
+		}
+
+		msg := tgbot.NewMessage(update.Message.Chat.ID, "")
+		switch update.Message.Command() {
+		case "help":
+			msg.Text = "Nuevo Gasto: /gasto \nVer budget: /budget"
+		case "budget":
+            msg.Text = "botones..."
+			msg.ReplyMarkup = inlineKeyboard
+		case "close":
+            msg.Text = "closing"
+			msg.ReplyMarkup = tgbot.NewRemoveKeyboard(true)
+		default:
+			msg.Text = "Comando desconocido"
+
+		}
 
 		if _, err = bot.Send(msg); err != nil {
 			panic(err)
